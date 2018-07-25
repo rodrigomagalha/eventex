74b75e43ec794630b73b77bfc05a83dcf8f5851f
Rodrigo Coelho de Magalhaes <Rodrigo@MacBook-de-Rodrigo.local>
2018-07-24 22:47:03 -0300

    Persiste a inscrição

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Rodrigo Magalhães',
+            cpf='997.893.160-03',
+            email='rodrigo@magalhaes.com',
+            phone='(88) 3520-8987'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_cretate_at(self):
+        """"Subscription must have an auto created_at attr."""
+        self.assertIsInstance(self.obj.created_at, datetime)
