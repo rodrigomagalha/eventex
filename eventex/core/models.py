ff714e78b8730142c4903a6b80ec08f2e25537a2
Rodrigo <rodrigo@maistisolucoes.com.br>
2018-09-07 21:23:58 -0300

    Speaker

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.shortcuts import resolve_url as r
 
-# Create your models here.
+class Speaker(models.Model):
+    name = models.CharField('nome', max_length=250)
+    slug = models.SlugField('slug')
+    description = models.TextField('descrição', blank=True)
+    photo = models.URLField('foto')
+    website = models.URLField('website')
+
+    class Meta:
+        verbose_name = 'palestrante'
+        verbose_name_plural = 'palestrantes'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return r('speaker_detail', slug=self.slug)
---
+++ eventex/core/tests/test_view_speaker_detail.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from django.shortcuts import resolve_url as r
+from eventex.core.models import Speaker
+
+
+class SpeakerDetailGet(TestCase):
+    def setUp(self):
+        Speaker.objects.create(
+            name='Grace Hopper',
+            description='Progamadora e almirante.',
+            photo='http://hbn.link/hopper-pic',
+            website='http://hbn.link/hopper-site',
+            slug='grace-hopper',
+        )
+        self.response = self.client.get(r('speaker_detail', slug='grace-hopper'))
+
+    def test_get(self):
+        """GET should return status 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'core/speaker_detail.html')
+
+    def test_html(self):
+        contents = [
+            'Grace Hopper',
+            'Progamadora e almirante.',
+            'http://hbn.link/hopper-pic',
+            'http://hbn.link/hopper-site',
+        ]
+
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
+    def test_context(self):
+        """Speaker must be in context"""
+        speaker = self.response.context['speaker']
+        self.assertIsInstance(speaker, Speaker)
+
+class SpeakerDetaulNotFount(TestCase):
+    def test_not_fond(self):
+        response = self.client.get(r('speaker_detail', slug='not-found'))
+        self.assertEqual(404, response.status_code)
