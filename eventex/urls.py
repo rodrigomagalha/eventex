cfd0a681e5596282c924ef1909ff25a45b944a41
Rodrigo <rodrigo@maistisolucoes.com.br>
2018-09-08 17:43:23 -0300

    Criado Talks

--- eventex/urls.py
+++ eventex/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from eventex.core.views import home, speaker_detail
+from eventex.core.views import home, speaker_detail, talk_list
 
 urlpatterns = [
     path('', home, name='home'),
     path('inscricao/', include('eventex.subscriptions.urls')),
     path('palestrantes/<slug:slug>/', speaker_detail, name='speaker_detail'),
+    path('palestras/', talk_list, name='talk_list'),
     path('admin/', admin.site.urls),
 
 ]
